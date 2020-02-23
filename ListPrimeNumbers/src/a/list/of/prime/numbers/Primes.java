2390ee95963892916e3ab253ea9a97de2b399f5f
hanifahsho <50223009+hanifahsho@users.noreply.github.com>
2020-02-23 10:06:38 +0700

    Add files via upload

---
+++ ListPrimeNumbers/src/a/list/of/prime/numbers/Primes.java
@@ -0,0 +1,25 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package a.list.of.prime.numbers;
+
+/**
+ *
+ * @author Hani
+ */
+import java.awt.*;
+import javax.swing.*;
+public class Primes
+{
+    public static void main (String[] args)
+    {
+        JFrame frame = new JFrame ("Primes");
+        frame.setDefaultCloseOperation (JFrame.EXIT_ON_CLOSE);
+        PrimePanel primePanel = new PrimePanel ();
+        frame.getContentPane().add(primePanel);
+        frame.pack();
+        frame.setVisible(true);
+    }
+}
